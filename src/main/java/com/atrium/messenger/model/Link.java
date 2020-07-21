8bff5aa9b13c2ce991db8a6b42eae892b3dde997
Ash <hrksingh17@gmail.com>
2020-07-21 19:44:56 +0530

    improvmeint in HATEOS impl

---
+++ src/main/java/com/atrium/messenger/model/Link.java
@@ -0,0 +1,12 @@
+package com.atrium.messenger.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Link{
+	private String link;
+	private String rel;
+}
+
--- src/main/java/com/atrium/messenger/model/Message.java
+++ src/main/java/com/atrium/messenger/model/Message.java
@@ -1,17 +1,19 @@
 package com.atrium.messenger.model;
 
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
-
 
 import javax.xml.bind.annotation.XmlTransient;
 
-
-import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
-@Data
+@Getter
+@Setter
 @NoArgsConstructor
 public class Message {
 	private long id;
@@ -19,14 +21,23 @@ public class Message {
 	private Date created;
 	private String author;
 
-	@XmlTransient
+	@Getter(onMethod = @__( @XmlTransient)) 
 	private Map<Long, Comment> comments = new HashMap<>();
+	
+	private List<Link> links = new ArrayList<>();
 
 	public Message(long id, String message, String author) {
 		this.id = id;
 		this.message = message;
 		this.author = author;
 		this.created = new Date();
+	}
+
+	public void addLink(String url, String rel) {
+		Link link = new Link();
+		link.setLink(url);
+		link.setRel(rel);
+		links.add(link);
 	}
 
 }
